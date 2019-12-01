88fd839339d685ea3704aa3b24bdb3f9a1792aa5
Victor Hugo Faria Dias Magalhães <victormagalhaes00@outlook.com>
2019-12-01 17:14:56 -0300

    Finalização da estrutura do método core

--- src/homework.py
+++ src/homework.py
@@ -1,11 +1,13 @@
 import sys
 from student import student
 
-
-
 def core():
     studentData = student(input("Write your name: "), input("Write your grade (e.g 1st, 2nd..): "), input("Write your teacher's name: "), input("Write your school's name: "))
     theme = input("Write the theme of your homework research: ")
+    searchData = search(theme)
+    themeTags = textProcessing(searchData)
+    images = imagesDownload(themeTags)
+    generateLatex(studentData, theme, searchData, images)
 
 if __name__ == '__main__':
     core()
