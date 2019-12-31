ad17d1469be50b53a9e9e2a1796a4414ef1ab0ae
Victor Hugo <victormagalhaes00@outlook.com>
2019-12-31 09:32:38 -0300

    finalizando a feature search

--- src/homework.py
+++ src/homework.py
@@ -13,16 +13,17 @@ def askCredentials():
         
 
 def core():
-    studentData = (input("Write your name: "), input("Write your grade (e.g 1st, 2nd..): "), input("Write your teacher's name: "), input("Write your school's name: "))
+    studentData = student(input("Write your name: "), input("Write your grade (e.g 1st, 2nd..): "), input("Write your teacher's name: "), input("Write your school's name: "))
+    print(studentData.getGrade())
     language = input("Write the language code of your research: (supported codes: en (english), pt(portuguese)): ")
     theme = input("Write the theme of your homework research: ")
     searchData = search(theme, language)
-    print(searchData['content'])
     with open('./result.txt', 'w+') as resultFile:
+        resultFile.write(studentData.getHeader())
+    with open('./result.txt', 'a') as resultFile:
         resultFile.write(searchData['content'])
-    #themeTags = textProcessing(searchData)
     #images = imagesDownload(themeTags)
-    #generateLatex(studentData, theme, searchData, images)
+    #generateWord(studentData, theme, searchData, images)
 
 if __name__ == '__main__':
     askCredentials()
